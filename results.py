f45438be9586c62d471a4f5a8e4c3be1dabb19e7
tensorcat <tensorcat@tensorcat.com>
2022-11-23 06:38:41 +0000

    in p rocess

--- results.py
+++ results.py
@@ -14,35 +14,44 @@ sql_getnamefromtag = "SELECT name FROM tags WHERE id = %s"
 sql_getuseridsfromtaguser = "SELECT tagid, userid FROM taguser"
 sql_getlinkswithtag = "SELECT t1, t2 FROM links WHERE t1 = %s OR t2 = %s"
 
+def outputCSV(_tagusers, _tags):
+  #output the final tag users
+  for tu in _tagusers:
+    if _tagusers[tu] < 2:
+      continue
+    print("\"" + _tags[tu] + "\", " + str(_tagusers[tu])) 
 
+  # Output users per tab\g      
+  for l in linkswithtag:
+    # print(str(tag) + ":" + str(l))
+    if _tagusers[l] > 1:
+      print("\"" + tags[tag] + "\",\"" + tags[l] + "\"" )
+
+# Build the tags list (id, name)
 mycursor.execute(sql_gettags)
 tmp = mycursor.fetchall()
 tags = {}
 for t in tmp:
     tags[t[0]] = t[1]
 
+# Build the users list (userid, name)
 mycursor.execute(sql_getusers)
 tmp = mycursor.fetchall()
 users = {}
 for t in tmp:
     users[t[0]] = t[1]
     
+# Build tagusers, for each tag count how many users have posted to it    
 tagusers = {}    
 mycursor.execute(sql_getuseridsfromtaguser)  
 tmp =  mycursor.fetchall()
-
 for t in tmp:
   if t[0] not in tagusers:                                               
     tagusers[t[0]] = 1
   else:
     tagusers[t[0]] += 1
 
-for tu in tagusers:
-  if tagusers[tu] < 2:
-    continue
-  print("\"" + tags[tu] + "\", " + str(tagusers[tu]))    
-
-# taglist = {}
+# Build link table
 for tag in tags:
   val = (tag,tag)
   mycursor.execute(sql_getlinkswithtag, val)
@@ -52,10 +61,10 @@ for tag in tags:
     if link[0] == tag:
       if link[1] not in links:
         linkswithtag.append(link[1])
-      else:
-        if link[0] not in links:
-          linkswithtag.append(link[0])
-  for l in linkswithtag:
-    # print(str(tag) + ":" + str(l))
-    if tagusers[l] > 1:
-      print("\"" + tags[tag] + "\",\"" + tags[l] + "\"" )
+    else:
+      if link[0] not in links:
+        linkswithtag.append(link[0])
+        
+#output to stdout
+outputCSV(tagusers, tags)
+
