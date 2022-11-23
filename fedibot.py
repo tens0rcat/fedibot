dea3318893cc3599b4606622be7f85dba1fa5f0a
tensorcat <tensorcat@tensorcat.com>
2022-11-23 03:30:00 +0000

    finish db refactor

--- fedibot.py
+++ fedibot.py
@@ -1,45 +1,59 @@
 from mastodon import Mastodon
 import mysql.connector
-from secrets.secrets import secrets
+from secrets.secrets import mastodonsecrets as sMasto
+from secrets.secrets import mysqlsecrets as sDB
 
-M_client_id = secrets['MASTODON_CLIENT_ID']
-M_api_base_url = secrets['MASTODON_PRIMARY_BASE_URL']
-M_email = secrets['MASTODON_PRIMARY_USER_EMAIL']
-M_user_token = secrets['MASTODON_PRIMARY_USER_TOKEN']
-M_login = secrets['MASTODON_PRIMARY_LOGIN']
+def mastodon_init():
+  M_client_id = sMasto['MASTODON_CLIENT_ID']
+  M_api_base_url = sMasto['MASTODON_PRIMARY_BASE_URL']
+  M_email = sMasto['MASTODON_PRIMARY_USER_EMAIL']
+  M_user_token = sMasto['MASTODON_PRIMARY_USER_TOKEN']
+  M_login = sMasto['MASTODON_PRIMARY_LOGIN']
 
-db = secrets['MYSQL_DATABASE']
-db_user = secrets['MYSQL_USER']
-db_pass = secrets['MYSQL_PASSWORD']
+  mastodon = Mastodon(
+    client_id = M_client_id,
+    api_base_url = M_api_base_url
+  )
+  mastodon.log_in(
+      M_email,
+      M_user_token,
+      M_login
+  )
+  return mastodon
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user=db_user,
-  password=db_pass,
-  database=db
-)
+def mastodon_getposts():
+  return mastodon.timeline_local() + mastodon.timeline_public()
 
-mycursor = mydb.cursor()
+mastodon = mastodon_init()
+posts = mastodon_getposts()
 
-mastodon = Mastodon(
-  client_id = M_client_id,
-  api_base_url = M_api_base_url
-)
-mastodon.log_in(
-    M_email,
-    M_user_token,
-    M_login
-)
+def db_init():
+  db = sDB['MYSQL_DATABASE']
+  db_user = sDB['MYSQL_USER']
+  db_pass = sDB['MYSQL_PASSWORD']
 
-posts = mastodon.timeline_local() 
-posts += mastodon.timeline_public()
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user=db_user,
+    password=db_pass,
+    database=db
+  )
+
+  return (mydb, mydb.cursor())
+
+retval = db_init()
+mydb = retval[0]
+mycursor = retval[1]
 
 sql_users = "INSERT IGNORE INTO users (id, name) VALUES (%s, %s)"
-#sql_posts = "INSERT IGNORE INTO posts (postid, userid) VALUES (%s, %s)"
+sql_posts = "INSERT IGNORE INTO posts (postid, userid) VALUES (%s, %s)"
 sql_tagname = "INSERT IGNORE INTO tags (name) VALUES (%s)"
 sql_tags = "INSERT INTO posttag (postid, tagid) VALUES (%s, %s)"
 sql_gettag = "SELECT id FROM tags WHERE name = %s"
 sql_taguser = "INSERT IGNORE INTO taguser (tagid, userid) VALUES (%s, %s)"
+sql_links = "INSERT IGNORE INTO links (t1, t2) VALUES (%s, %s)"
+sql_countlinks = "SELECT COUNT(*) FROM links WHERE t1 = %s AND t2 = %s"
+sql_inclinkcount = "UPDATE links SET count=count+1 where t1 = %s and t2 = %s"
 
 for post in posts:
   # print("NewPost " + str(post.id) + " by: " + str(post.account.id) + " - " + post.account.acct)
@@ -48,10 +62,15 @@ for post in posts:
   val = (post.account.id, post.account.acct)
   mycursor.execute(sql_users, val)
   mydb.commit()
+  if mycursor.rowcount == 0:
+    pass
   # print(mycursor.rowcount, "user record inserted.")
-  # val = (post.id, post.account.id)
-  # mycursor.execute(sql_posts, val)
-  # mydb.commit()
+  val = (post.id, post.account.id)
+  mycursor.execute(sql_posts, val)
+  mydb.commit()
+  if mycursor.rowcount == 0:
+    # print("duplicate " + str(post.id))
+    continue
   # print(mycursor.rowcount, "post record inserted.")
   for tag in post.tags:
     # print(str(post.id) + " " + tag.name)
@@ -68,13 +87,30 @@ for post in posts:
     val = (tagid[0][0], post.account.id)
     mycursor.execute(sql_taguser, val)
     mydb.commit()
+  for tag in post.tags:
+    val = (tag.name,)
+    mycursor.execute(sql_gettag, val)
+    tmp1 = mycursor.fetchone()[0]
+    for t in post.tags:
+      val = (t.name,)
+      mycursor.execute(sql_gettag, val)
+      tmp2 = mycursor.fetchone()[0]
+      t1 = max(tmp1, tmp2)
+      t2 = min(tmp1, tmp2)
+      if t1 == t2:
+        continue
+      val = (t1, t2)
+      mycursor.execute(sql_links, val)
+      mydb.commit()
+      # mycursor.execute(sql_inclinkcount, val)
+      # mydb.commit()
     #   mycursor.execute(sql_gettag, val)
     #   tagid = mycursor.fetchall()
     # #for t in tagresult:
     #   # print(tag.name + ": " + str(t[0]))
     # if len(tagid) > 1:
     #   print("Duplicate TAG ERROR: " + tag.name)
-    val = (post.id, tagid[0][0])
-    mycursor.execute(sql_tags, val)
-    mydb.commit()
+    # val = (post.id, tagid[0][0])
+    # mycursor.execute(sql_tags, val)
+    # mydb.commit()
     # # print(mycursor.rowcount, "posttag")
--- results.py
+++ results.py
@@ -1,20 +1,24 @@
 import mysql.connector
-from secrets.secrets import secrets
+from secrets.secrets import mysqlsecrets as sDB
 from collections import Counter
-from itertools import chain
 
-db = secrets['MYSQL_DATABASE']
-db_user = secrets['MYSQL_USER']
-db_pass = secrets['MYSQL_PASSWORD']
+def db_init():
+  db = sDB['MYSQL_DATABASE']
+  db_user = sDB['MYSQL_USER']
+  db_pass = sDB['MYSQL_PASSWORD']
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user=db_user,
-  password=db_pass,
-  database=db
-)
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user=db_user,
+    password=db_pass,
+    database=db
+  )
 
-mycursor = mydb.cursor()
+  return (mydb, mydb.cursor())
+
+retval = db_init()
+mydb = retval[0]
+mycursor = retval[1]
 
 sql_gettags = "SELECT * FROM tags"
 sql_getusers = "SELECT * FROM users"
@@ -24,6 +28,7 @@ sql_getuserfrompost = "SELECT userid FROM posts WHERE postid = %s"
 sql_getnamefromusers = "SELECT name FROM users WHERE id = %s"
 sql_getnamefromtag = "SELECT name FROM tags WHERE id = %s"
 sql_getuseridsfromtaguser = "SELECT tagid, userid FROM taguser"
+sql_getlinkswithtag = "SELECT t1, t2 FROM links WHERE t1 = %s OR t2 = %s"
 
 
 mycursor.execute(sql_gettags)
@@ -43,39 +48,55 @@ mycursor.execute(sql_getuseridsfromtaguser)
 tmp =  mycursor.fetchall()
 
 for t in tmp:
-    if t[0] not in tagusers:                                               
-        tagusers[t[0]] = 1
-    else:
-        tagusers[t[0]] += 1
+  if t[0] not in tagusers:                                               
+    tagusers[t[0]] = 1
+  else:
+    tagusers[t[0]] += 1
 
 for tu in tagusers:
-    if tagusers[tu] > 1:
-        print("\"" + tags[tu] + "\", " + str(tagusers[tu]))    
+  if tagusers[tu] > 1:
+      print("\"" + tags[tu] + "\", " + str(tagusers[tu]))    
 
 # taglist = {}
 for tag in tags:
-    basetag = tag
-    val = (basetag,)
-    mycursor.execute(sql_getpostswithtag,val)
-    postswithtags = mycursor.fetchall()
+  val = (tag,tag)
+  mycursor.execute(sql_getlinkswithtag, val)
+  links = mycursor.fetchall()
+  linkswithtag = []
+  for link in links:
+    if link[0] == tag:
+      if link[1] not in links:
+        linkswithtag.append(link[1])
+      else:
+        if link[0] not in links:
+          linkswithtag.append(link[0])
+  for l in linkswithtag:
+    # print(str(tag) + ":" + str(l))
+    if tagusers[l] > 1:
+      print("\"" + tags[tag] + "\",\"" + tags[l] + "\"" )
+      
+
+
+    # mycursor.execute(sql_getpostswithtag,val)
+    # postswithtags = mycursor.fetchall()
     # taglist[basetag] = {}
     # usercount[basetag] = 0
     # users = []
-    for post in postswithtags:
+    # for post in postswithtags:
+    # #     val = (post[0],)
+    # #     mycursor.execute(sql_getuserfrompost, val)
+    # #     user = mycursor.fetchall()
+    # #     if user[0][0] not in users:
+    # #         users.append(user[0][0])
+    # #         usercount[basetag] += 1
     #     val = (post[0],)
-    #     mycursor.execute(sql_getuserfrompost, val)
-    #     user = mycursor.fetchall()
-    #     if user[0][0] not in users:
-    #         users.append(user[0][0])
-    #         usercount[basetag] += 1
-        val = (post[0],)
-        mycursor.execute(sql_gettagsfrompost, val)
-        adjacenttags = mycursor.fetchall()
-        for atag in adjacenttags:
-            if atag[0] == basetag: 
-                continue
-            if tagusers[atag[0]] > 1:
-                print("\"" + tags[tag] + "\", \"" + tags[atag[0]] + "\"")
+    #     mycursor.execute(sql_gettagsfrompost, val)
+    #     adjacenttags = mycursor.fetchall()
+    #     for atag in adjacenttags:
+    #         if atag[0] == basetag: 
+    #             continue
+    #         if tagusers[atag[0]] > 1:
+    #             print("\"" + tags[tag] + "\", \"" + tags[atag[0]] + "\"")
         #     if atag[0] > basetag:
         #         t1 = basetag
         #         t2 = atag[0]
