fe6a3dc24fa4995a37d87d892750e5953f9e3d17
tensorcat <tensorcat@tensorcat.com>
2022-11-26 07:19:57 +0000

    so I don't have to keep logging onto the database cli to dump data

---
+++ purgealldata.py
@@ -0,0 +1,9 @@
+from modules import Mysql as mysql
+
+retval = mysql.init()
+mydb = retval[0]
+mycursor = retval[1]
+
+#sql_purge = "TRUNCATE `links`; TRUNCATE `posts`; TRUNCATE `tags`; TRUNCATE `taguser`; TRUNCATE `users`;"
+
+mycursor.execute(sql_purge)
