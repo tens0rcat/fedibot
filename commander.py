f07290dd359fe20e257674df771a0f9f643b0274
tensorcat <tensorcat@tensorcat.com>
2022-12-05 03:47:33 +0000

    little prototype thing to follow a list of users from a text file.

--- commander.py
+++ commander.py
@@ -1,26 +1,57 @@
 from dataclasses import dataclass
-from modules import myMastodon as mastodon
+from modules import myMastodon as Mastodon
 from mysecrets.fedibotsecrets import mastodonsecrets as M_sec
+import requests
+import json
+import time
 
-#initialize myMastodon module with the name of the app and the secrets
-mastodon.init("commander", M_sec, "dev")
+local = Mastodon.init("commander", "freebot.dev", "tensorcat", M_sec)
+local = Mastodon.login("pace")
 
-#login to the instance, required if doing anything except reading public feeds (local and remote)
-mastodon = mastodon.login()
+big = Mastodon.init("commander", "nerdculture.de", "tensorcat", M_sec)
+big = Mastodon.login()
 
 #Get my account data
-mydata = mastodon.me()
+mydata = local.me()
+followers = local.account_following(id = mydata.id)
+followers = local.fetch_remaining(followers)
 
-file = open("files/data/newsies", "r")
+file = open("files/data/newsies", "r") #file of usernames to follow
 lines = file.readlines()
 file.close()
+
+kwargs = {}
+kwargs["headers"] = {
+  'Authorization': "Bearer " + local.access_token,
+  'User-Agent' : "api"
+}
+
 for line in lines:
   username = line[0:len(line)-1].strip(" ")
+  userinlist = (0 != len(list(filter(lambda f: f["acct"] == username, followers))))
+  if (userinlist): 
+    continue
+  time.sleep(5)
   print(username, end=" - ")
-  acct = mastodon.account_search(username)
+  #acct = local.account_search("@" + username)
+
+  url = "https://freebot.dev/api/v2/search?q=" + username + "&resolve=true"
+  response = requests.get(url, **kwargs)
+  try:
+    acct = json.loads(response.text)['accounts']
+  except:
+    acct = {}
+
   if len(acct) == 0:
     print("Not Found")
     continue
-  mastodon.account_follow(acct[0].id,False, False)
+  for a in acct:
+    try:
+      id = a['id']
+      rel = local.account_follow(id, False, False) #so does a "remote" account need to be created and try again?
+      print("Followed")
+    except BaseException as e:
+      print("Follow Failed")
+      print(e)
 #  not working
 pass
