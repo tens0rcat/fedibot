0f526efc4d277496e8a085842415f05e3a3e2da2
tensorcat <tensorcat@tensorcat.com>
2022-12-11 21:46:36 +0000

    stuff

--- purgealldata.py
+++ purgealldata.py
@@ -4,6 +4,10 @@ retval = mysql.init()
 mydb = retval[0]
 mycursor = retval[1]
 
+sql_purge = ""
 #sql_purge = "TRUNCATE `links`; TRUNCATE `posts`; TRUNCATE `tags`; TRUNCATE `taguser`; TRUNCATE `users`;"
 
-mycursor.execute(sql_purge)
+if (sql_purge == ""):
+  print("If this is really what you want to do then uncomment the line above")
+else:
+  mycursor.execute(sql_purge)
